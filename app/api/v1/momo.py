1559b2debaf459b0b7dceef7d56cc6525cbc63c5
cuongnn13 <cuongnn13@fpt.com>
2025-02-24 15:37:14 +0700

    oke

--- app/api/v1/momo.py
+++ app/api/v1/momo.py
@@ -8,6 +8,9 @@ import requests
 import hmac
 import hashlib
 
+from app.extensions import db
+from app.models import PaymentMomo
+
 api = Blueprint('momo', __name__)
 
 endpoint = "https://test-payment.momo.vn/v2/gateway/api/create"
@@ -20,8 +23,7 @@ requestType = "payWithMethod"
 extraData = ""  # pass empty value or Encode base64 JsonString
 autoCapture = True
 lang = "vi"
-redirectUrl = f"{CONFIG.BASE_URL_WEBSITE}/api/v1/momo/payment_return"
-ipnUrl = f"{CONFIG.BASE_URL_WEBSITE}/api/v1/momo/payment_notify"
+
 storeId = "Test Store"
 orderGroupId = ""
 
@@ -33,6 +35,8 @@ def create_payment():
         amount = "60000"
         orderId = str(uuid())
         requestId = str(uuid())
+        redirectUrl = f"{CONFIG.BASE_URL_WEBSITE}/api/v1/momo/{orderId}/{requestId}/payment_return"
+        ipnUrl = f"{CONFIG.BASE_URL_WEBSITE}/api/v1/momo/{orderId}/{requestId}/payment_notify"
         rawSignature = "accessKey=" + accessKey + "&amount=" + amount + "&extraData=" + extraData + "&ipnUrl=" + ipnUrl + "&orderId=" + orderId \
                        + "&orderInfo=" + orderInfo + "&partnerCode=" + partnerCode + "&redirectUrl=" + redirectUrl \
                        + "&requestId=" + requestId + "&requestType=" + requestType
@@ -65,18 +69,48 @@ def create_payment():
         # Trả về phản hồi
         data = response.json()
 
+        payment_momo = PaymentMomo(id=str(uuid()),order_momo_id=orderId, request_momo_id=requestId)
+        db.session.add(payment_momo)
+        db.session.flush()
+        db.session.commit()
+
         return send_result(data=data)
     except Exception as ex:
+        db.session.rollback()
         return send_error(message=str(ex))
 
 
-@api.route('/payment_return', methods=['GET'])
-def payment_return():
-    response_data = request.args.to_dict()
-    return send_result(data=response_data)
+@api.route('/<order_momo_id>/<request_momo_id>/payment_return', methods=['GET'])
+def payment_return(order_momo_id, request_momo_id):
+    try:
+        response_data = request.args.to_dict()
+        # payment_momo = PaymentMomo.query.filter(PaymentMomo.order_momo_id == order_momo_id,
+        #                          PaymentMomo.request_momo_id == request_id).first()
+        # if payment_momo:
+        #     payment_momo.result = response_data
+        #     db.session.flush()
+        #     db.session.commit()
+        return send_result(data=response_data)
+
+    except Exception as ex:
+        db.session.rollback()
+        return send_error(message=str(ex))
 
 # API xử lý thông báo thanh toán từ Momo (dành cho backend)
-@api.route('/payment_notify', methods=['POST'])
-def payment_notify():
-    data = request.json
-    return send_result(data=data)
+@api.route('/<order_momo_id>/<request_momo_id>/payment_notify', methods=['POST'])
+def payment_notify(order_momo_id, request_momo_id):
+    try:
+        data = request.get_json()
+        print("đã vào BE", data)
+        payment_momo = PaymentMomo.query.filter(PaymentMomo.order_momo_id == order_momo_id,
+                                                PaymentMomo.request_momo_id == request_momo_id).first()
+        if payment_momo:
+            payment_momo.result = data
+            db.session.flush()
+            db.session.commit()
+
+        return send_result(data=data)
+
+    except Exception as ex:
+        db.session.rollback()
+        return send_error(message=str(ex))
