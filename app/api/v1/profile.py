3f400c64eccadf7f83c03c40c7ef9b3beeecc4dc
cuongnn13 <cuongnn13@fpt.com>
2025-03-31 09:56:18 +0700

    Oke

--- app/api/v1/profile.py
+++ app/api/v1/profile.py
@@ -13,7 +13,7 @@ from app.api.helper import get_permissions, CONFIG, send_email_template, get_rol
 from app.api.helper import send_error, send_result
 from app.extensions import jwt, db
 from app.models import User, EmailTemplate, VerityCode, Mail, Address, Article, Community
-from app.utils import trim_dict, get_timestamp_now, logged_input, data_preprocessing, generate_random_number_string, \
+from app.utils import trim_dict, get_timestamp_now, data_preprocessing, generate_random_number_string, \
     body_mail, escape_wildcard
 from app.extensions import mail
 
