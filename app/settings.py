f085dddc3a4d2c384c85bddb9db2c6b6ebc9a472
cuongnn13 <cuongnn13@fpt.com>
2025-04-18 15:51:37 +0700

    Oke

--- app/settings.py
+++ app/settings.py
@@ -11,7 +11,7 @@ class Config(object):
 
 class DevConfig(Config):
     """Development configuration."""
-    # Test deploy 1
+    # Biết thế
     name_file_env = '.env.dev'
     # name_file_env = '.env.test'
 
