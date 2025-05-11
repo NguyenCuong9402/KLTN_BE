f971d53df6c157d87e338996785a4661165abbfc
Nguyen Ngoc Cuong <cuong09042002@gmail.com>
2025-05-12 01:07:02 +0700

    oke

--- app/scheduler_task/backup_data.py
+++ app/scheduler_task/backup_data.py
@@ -6,31 +6,36 @@ import os
 def __thread_backup():
     with db.app.app_context():
         # export mysql
-        logger.info("Đã chạy vào backup----------------------")
-        logger.info("backup mysql----------------------")
 
-        backup_dbsql_name = f"{CONFIG.BK_DBNAME_MYSQL}_{str(get_datetime_now().date())}.sql"
-        os.system(f"mysqldump -c -P {CONFIG.BK_PORT_MYSQL} -h {CONFIG.BK_HOST_MYSQL} -u {CONFIG.BK_USERNAME_MYSQL} "
-                  f"--password={CONFIG.BK_PASSWORD_MYSQL} {CONFIG.BK_DBNAME_MYSQL} > {backup_dbsql_name}")
-        # export mongo
-        # logger.info("backup mongo----------------------")
-        #
-        # backup_mongodb_name = f"{CONFIG.MONGO_DB}_{str(get_datetime_now().date())}"
-        # # password_mongo = urllib.parse.unquote(CONFIG.BK_PASSWORD_MONGODB)
-        # uri = f"mongodb://{CONFIG.BK_USERNAME_MONGODB}:{CONFIG.BK_PASSWORD_MONGODB}@{CONFIG.BK_HOST_MONGODB}:{CONFIG.BK_PORT_MONGODB}/?authMechanism=SCRAM-SHA-256"
-        # os.system(f"mongodump --uri=\"{uri}\" --out={backup_mongodb_name}")
+        try:
+            logger.info("Đã chạy vào backup----------------------")
+            logger.info("backup mysql----------------------")
 
-        # zip folder backup
-        backup_folder_name = f"backup_{str(get_datetime_now().date())}.zip"
-        image_path = './app/files/backup'
-        os.makedirs(image_path, exist_ok=True)
+            backup_dbsql_name = f"{CONFIG.BK_DBNAME_MYSQL}_{str(get_datetime_now().date())}.sql"
+            os.system(f"mysqldump -c -P {CONFIG.BK_PORT_MYSQL} -h {CONFIG.BK_HOST_MYSQL} -u {CONFIG.BK_USERNAME_MYSQL} "
+                      f"--password={CONFIG.BK_PASSWORD_MYSQL} {CONFIG.BK_DBNAME_MYSQL} > {backup_dbsql_name}")
+            # export mongo
+            # logger.info("backup mongo----------------------")
+            #
+            # backup_mongodb_name = f"{CONFIG.MONGO_DB}_{str(get_datetime_now().date())}"
+            # # password_mongo = urllib.parse.unquote(CONFIG.BK_PASSWORD_MONGODB)
+            # uri = f"mongodb://{CONFIG.BK_USERNAME_MONGODB}:{CONFIG.BK_PASSWORD_MONGODB}@{CONFIG.BK_HOST_MONGODB}:{CONFIG.BK_PORT_MONGODB}/?authMechanism=SCRAM-SHA-256"
+            # os.system(f"mongodump --uri=\"{uri}\" --out={backup_mongodb_name}")
 
-        os.system(f"zip -r {backup_folder_name} {backup_dbsql_name} {image_path}")
-        # upload backup to s3
+            # zip folder backup
+            backup_folder_name = f"backup_{str(get_datetime_now().date())}.zip"
+            image_path = './app/files/backup'
+            os.makedirs(image_path, exist_ok=True)
+
+            os.system(f"zip -r {backup_folder_name} {backup_dbsql_name} {image_path}")
+            # upload backup to s3
 
 
-        # Remove file created quá 3 ngày trong image_path
-        os.system(f"find {image_path} -type f -mtime +3 -exec rm {{}} \;")
+            # Remove file created quá 3 ngày trong image_path
+            os.system(f"find {image_path} -type f -mtime +3 -exec rm {{}} \;")
+        except Exception as ex:
+            logger.info(f"{str(ex)}")
+
 
         # remove backup local in docker
         # os.system(f"rm -rf {backup_mongodb_name}")
